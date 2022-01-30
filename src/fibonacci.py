a4d6c045fbd3c45c62792963809f90e2b851bc2f
Buzovskyi Vitalii <buzovskiy.v@gmail.com>
2022-01-30 09:28:33 +0200

    initial commit

---
+++ src/fibonacci.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+
+def fib(n):
+    """
+    Get Fibonacci number
+    :param n: integer
+    :return: integer
+    """
+    if n <= 2:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+
+if __name__ == '__main__':
+    print(fib(7))
