7773a2d03a6bec44c3333931ecd70dddc4c2227f
Vitaliy Buzovskiy <buzovskiy@gmail.com>
2022-02-09 13:30:03 +0200

    add new two sum

--- app/leetcode/two_sum.py
+++ app/leetcode/two_sum.py
@@ -1,7 +1,25 @@
 class Solution:
-    def twoSum(self, nums: list, target: int) -> list:
+
+    @classmethod
+    def twoSum(cls, nums: list, target: int) -> list:
         for i1, el1 in enumerate(nums):
             for i2, el2 in enumerate(nums):
                 if i1 != i2 and el1 + el2 == target:
                     return [i1, i2]
 
+    @classmethod
+    def two_sum_enhanced(cls, nums: list, target: int) -> list:
+        for i in range(len(nums)):
+            el2 = target - nums[i]
+            el2_inds = [i2 for i2, _el2 in enumerate(nums) if _el2 == el2 and i2 != i]
+            if len(el2_inds):
+                return [i, el2_inds[0]]
+
+    @classmethod
+    def two_sum_best(cls, nums: list, target: int) -> list:
+        element_maps = {}
+        for i in range(len(nums)):
+            if nums[i] in element_maps:
+                return [element_maps[nums[i]], i]
+            else:
+                element_maps[target - nums[i]] = i
