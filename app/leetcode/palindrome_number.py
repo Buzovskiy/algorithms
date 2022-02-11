ba758c5cea1c2ca83a93d98a421cf0958b7400e9
Buzovskyi Vitalii <buzovskiy.v@gmail.com>
2022-02-11 02:04:23 +0200

    palindrome

--- app/leetcode/palindrome_number.py
+++ app/leetcode/palindrome_number.py
@@ -10,3 +10,33 @@ class Solution:
             return True
         else:
             return False
+
+    numbers = []
+    number_input = None
+
+    def isPalindrome2(self, x: int) -> bool:
+        if x < 0:
+            return False
+        if self.number_input is None:
+            self.number_input = x
+        remainder = x % 10
+        x = int((x - remainder) / 10)
+        self.numbers.append(remainder)
+        if x / 10 >= 0.1:
+            return self.isPalindrome2(x)
+        else:
+            num = 0
+            for (i, _) in enumerate(self.numbers):
+                num += _ * (10 ** (len(self.numbers)-(i+1)))
+            if self.number_input == num:
+                return True
+            return False
+
+print(Solution().isPalindrome2(0))
+
+
+
+
+
+# isPalindrom(100001)
+# print(numbers)
